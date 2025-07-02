16a316f2b475b91971701a56e1e8596169ebbc7c
Caetano V. Mantovani <158756564+crowuete@users.noreply.github.com>
2025-07-02 16:51:09 -0300

    falta trocar o for por um while e fazer o main()

--- arrumando_tarefas.py
+++ arrumando_tarefas.py
@@ -24,10 +24,24 @@ For each test case output a line with an integer representing the minimum lost m
 # Falta o main()
 
 def arrumando_tarefas(tempo_total: int, tarefas: list[tuple[int, int]]) -> int:
-    tarefas_ordenadas: list[tuple[int, int]] = sorted(tarefas, key=lambda tupla: tupla[0])
+    tarefas_ordenadas: list[tuple[int, int]] = sorted(tarefas, key=lambda tupla: tupla[0], reverse=True)
+    print(tarefas_ordenadas)
     valor_tarefas_total: int = sum(map(lambda tupla: tupla[0], tarefas_ordenadas))
     tarefas_escolhidas: list[int] = [0 for _ in range(tempo_total)]
-    # falta só pegar as tarefas ordenadas e preencher na lista tarefas escolhidas
-    # ideia: pegar e colocar no maior tempo possível
+    print(tarefas_escolhidas)
+    # vou transformar esse for em um while, pois há casos que não estão sendo tratados por causa do tempo_total.
+    # while que vai até tamanho total da lista *tarefas ou se a lista estiver completa (ex: variavel n de adições == len(tarefas_escolhidas))
+    for i in range(0, tempo_total):
+        tempo_tarefa_atual = tarefas_ordenadas[i][1] - 1
+        adicionado: bool = False
+        while not adicionado and 0 <= tempo_tarefa_atual:
+            if tarefas_escolhidas[tempo_tarefa_atual] == 0:
+                tarefas_escolhidas[tempo_tarefa_atual] = tarefas_ordenadas[i][0]
+                adicionado = True
+            tempo_tarefa_atual -= 1
+    print(tarefas_escolhidas)
     valor_tarefas_escolhidas: int = sum(tarefas_escolhidas)
     return valor_tarefas_total - valor_tarefas_escolhidas
+
+print(arrumando_tarefas(3, [(5, 1), (10, 2), (20, 3)]))
+print(arrumando_tarefas(2, [(1, 2), (2, 1), (4, 1), (2, 2)]))
